59f6a8865760cf99abac7fb295f5e5e270a67d61
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2025-08-11 19:07:55 +0000

    Missing Copyright Information in some files Fixes #2

--- classes/condition.php
+++ classes/condition.php
@@ -26,7 +26,7 @@ use restore_treasurehunt_activity_task;
  * Documentation: {@link https://moodledev.io/docs/apis/plugintypes/availability}
  *
  * @package    availability_treasurehunt
- * @copyright  2025 YOUR NAME <your@email.com>
+ * @copyright  2025 Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class condition extends \core_availability\condition {
